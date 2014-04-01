362c61ea5776030830f6b470e12df0de942df3df
root <root@ubuntu-VirtualBox.(none)>
2014-04-01 00:47:53 -0400

    Ready to add Q3 implementations

--- frontend.go
+++ frontend.go
@@ -16,12 +16,13 @@ import(
 //http://ec2-54-85-193-234.compute-1.amazonaws.com:8080/tweets/12002667192014-01-22%2012:21:45/about_tweet
 var dsn = "cloud9:gradproject@tcp(ec2-54-198-107-252.compute-1.amazonaws.com:3306)/TWEET_DB?parseTime=true"
 var hbaseServer = "http://ec2-54-85-193-234.compute-1.amazonaws.com:8080"
+
 var TEAM_ID, AWS_ACCOUNT_ID = "cloud9", "4897-8874-0242"
 var db *sql.DB 
 const layout = "2006-01-02 15:04:05"
-const header = TEAM_ID+","+AWS_ACCOUNT_ID+"\n"
-const mysql = 0
-const hbase = 1
+//WHICH BACKEND AM I USING???
+const mysql = false
+//???????????????????????????
 
 func q1(w http.ResponseWriter, r *http.Request){	
 	timeNow := time.Now().Format(layout)	
@@ -29,10 +30,69 @@ func q1(w http.ResponseWriter, r *http.Request){
   fmt.Println("Q1 HEARTBEAT at "+timeNow)
 }
 
+func q2(w http.ResponseWriter, r *http.Request){
+	//Extract values from URL
+	values := r.URL.Query()
+	userId := values["userid"][0]
+	tweetTime := values["tweet_time"][0]
+	fmt.Println("Q2 REQUEST: with userid="+userId+", tweet_time="+tweetTime)	
+	//Begin response
+	response := TEAM_ID+","+AWS_ACCOUNT_ID+"\n"	
+	if mysql {
+		response += q2mysql(userId, tweetTime)
+	}else{
+		response += q2hbase(userId, tweetTime)
+	}	
+	//Send response
+	fmt.Fprintf(w, response)
+	fmt.Println("Q2 RESPONSE:"+response)//Print to console what we're returning
+	
+}
+
+func q3(w http.ResponseWriter, r *http.Request){
+	userId := r.URL.Query()["userid"][0]
+	fmt.Println("Q3 REQUEST: with userid="+userId)
+	response := TEAM_ID+","+AWS_ACCOUNT_ID+"\n"	
+	if mysql {
+		response += q3mysql(userId)
+	}else{
+		response += q3hbase(userId)
+	}			
+	fmt.Fprintf(w, response)
+	fmt.Println("Q3 REQUEST:"+response)
+}
 /*
-* Implementation for MySQL backend
+* The server attaches handlers and listens for REST requests on port 80
 */
-func queryMySQL(userId string, tweetTime string) (response string){
+func main(){
+	fmt.Println("Frontend starting using "+backend()+" for the backend...")
+	if mysql {
+		var err error
+		db, err = sql.Open("mysql", dsn);
+		if err != nil {
+			log.Fatal(err)
+		}else{
+			fmt.Println("Database open!")
+		}	
+	}
+  	http.HandleFunc("/q1", q1)
+	http.HandleFunc("/q2", q2)
+	http.HandleFunc("/q3", q3)
+  	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func backend()(str string){
+	if mysql{
+		return "MySQL"
+	}else{
+		return "HBase"
+	}
+}
+
+/*
+* Implementation for Q2 MySQL backend
+*/
+func q2mysql(userId string, tweetTime string) (response string){
 	var tweetId uint64		
 	//Find tweet_id for given userid and tweettime		
 	rows, err := db.Query("SELECT id FROM tweets WHERE userid='"+userId+"' and created_at='"+tweetTime+"' ORDER BY id;")	
@@ -53,18 +113,15 @@ func queryMySQL(userId string, tweetTime string) (response string){
 		//Catch lingering errors
 		if err := rows.Err(); err != nil {
             log.Print(err)	
-    }			
+    	}			
 	}	
 	return response
 }
-/*
-* Implementation for HBase backend
-*/
 
- func queryHBase(userId string, tweetTime string) (response string){
-	//userId = "1200266719"
-	//tweetTime= 2014-01-22 12:21:45
-	// 
+/*
+* Implementation for Q2 sHBase backend
+*/
+ func q2hbase(userId string, tweetTime string) (response string){	
  	//Send GET request to HBase Stargate server
  	res, err := http.Get(hbaseServer+"/tweets/"+userId+tweetTime+",/about_tweet")
 
@@ -87,61 +144,10 @@ func queryMySQL(userId string, tweetTime string) (response string){
 	return response
 }
 
-func q2Query(userId string, tweetTime string, backend int){
-	
+func q3mysql(userId string) (response string){
+	return "Q3 MySQL backend not implemented yet"
 }
 
-
-func q2(w http.ResponseWriter, r *http.Request){
-	//Extract values from URL
-	values := r.URL.Query()
-	userId := values["userid"][0]
-	tweetTime := values["tweet_time"][0]
-	fmt.Println("Q2 REQUEST: with userid="+userId+", tweet_time="+tweetTime)
-	
-	//Begin response
-	response := TEAM_ID+","+AWS_ACCOUNT_ID+"\n"	
-
-	//Query MySQL
-	//response += queryMySQL(userId, tweetTime)	
-	
-	//Query HBase
-	//response += queryHBase(userId, tweetTime)
-
-	response += q2Query(userId, tweetTime, mysql)
-
-	//Send response
-	fmt.Println("Q2 RESPONSE:"+response)//Print to console what we're returning
-	fmt.Fprintf(w, response)
+func q3hbase(userId string) (response string){
+	return "Q3 HBase backend not implemented yet"
 }
-
-func q3(w http.ResponseWriter, r *http.Request){
-		userId := r.URL.Query()["userid"][0]
-
-		fmt.Println("Q3 REQUEST: with userid="+userId)
-
-		response := header
-		
-		response += q3Query(userId, mysql)
-
-		fmt.Fprintf(w, response)
-		fmt.Println("Q3 REQUEST:"+response)
-}
-
-func main(){
-	fmt.Println("Starting up the frontend now...")
-
-	// var err error
-	// db, err = sql.Open("mysql", dsn);
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }else{
-	// 	fmt.Println("Database open!")
-	// }	
-  http.HandleFunc("/q1", q1)
-	http.HandleFunc("/q2", q2)
-	http.HandleFunc("/q3", q3)
-  log.Fatal(http.ListenAndServe(":80", nil))
-}
-
-
